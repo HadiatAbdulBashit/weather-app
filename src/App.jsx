ce4a22933cee07edd58328f50c51013b0a5aae96
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2024-01-29 23:39:54 +0700

    change title, logo, style forecast and add search

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,52 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Select from "react-select";
 import axios from "axios";
 import moment from "moment";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState({});
   const [forecast, setForecast] = useState([]);
+  const [location, setLocation] = useState({});
+  const [selectedPlace, setSelectedPlace] = useState({ value: "auto:ip" });
+  const [optionSelect, setOptionSelect] = useState([]);
 
+  const promiseOptions = async (searchKey) => {
+    try {
+      const apiKey = "0cf0fea2677b42f899690517242901"; // API key
+      const response = await axios.get(
+        `http://api.weatherapi.com/v1/search.json?q=${
+          searchKey ? searchKey : "auto:ip"
+        }&key=${apiKey}`
+      );
 
-  useEffect(() => {
+      console.log(response);
+
+      const formattedOptions = response.data.map((location) => ({
+        value: location.url,
+        label: `${location.name} - ${location.region}, ${location.country}`,
+      }));
+
+      console.log(formattedOptions);
+
+      setOptionSelect(formattedOptions);
+    } catch (error) {
+      console.error("Error fetching options:", error);
+      setOptionSelect([]);
+    }
+  };
+
+  const handleChange = (selectedOption) => {
+    setSelectedPlace(selectedOption);
+  };
+
+  const handleInputChange = (inputValueSearchSelect) => {
+    promiseOptions(inputValueSearchSelect ?? "auto:ip");
+  };
+
+  const getWeather = () => {
     // Link API dari weatherapi untuk menampilkan data
     const apiKey = "0cf0fea2677b42f899690517242901"; // API key
-    const forecastUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=auto:ip&days=6`;
+    const forecastUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${selectedPlace.value}&days=6`;
 
     // Mengambil data ramalan cuaca saat ini dan 5 hari ke depan
     axios
@@ -20,10 +54,15 @@ function App() {
       .then((response) => {
         setForecast(response.data.forecast.forecastday);
         setCurrentWeather(response.data.current);
+        setLocation(response.data.location);
         console.log(response.data);
       })
       .catch((error) => console.error("Error fetching forecast:", error));
-  }, []);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, [selectedPlace]);
 
   return (
     <>
@@ -39,17 +78,15 @@ function App() {
             Weather App
           </a>
           <div className="d-flex gap-2">
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="City name, US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude"
-                aria-label="City name, US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude"
-              />
-              <button className="btn btn-secondary" type="button">
-                Search
-              </button>
-            </div>
+            <Select
+              value={selectedPlace}
+              onChange={handleChange}
+              onInputChange={handleInputChange}
+              placeholder="Type to search a location"
+              isSearchable
+              options={optionSelect}
+              className="search-navbar"
+            />
             <button
               className="navbar-toggler collapsed"
               type="button"
@@ -77,8 +114,8 @@ function App() {
           </div>
         </div>
       </nav>
-      <div className="container">
-        <h1>Weather App</h1>
+      <div className="container my-3">
+        <h1>{`${location.name} - ${location.region}, ${location.country}`}</h1>
 
         <h2>Today's Weather</h2>
         <div className="row">
@@ -101,25 +138,27 @@ function App() {
             </div>
           </div>
         </div>
-
-        <h2>5-Day Forecast</h2>
-        <div className="row">
+        <div className="my-3 g-3 row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 justify-content-center">
           {forecast.slice(1).map((day) => (
             <div className="col" key={day.date}>
-              <div className="card">
+              <div className="card text-center">
+                <div className="card-header">
+                {moment(day.date).format("dddd")}
+                </div>
                 <div className="card-body">
-                  <div className="card-title">
-                    {moment(day.date).format("dddd")}
-                  </div>
                   <img
                     src={currentWeather.condition?.icon}
                     alt={currentWeather.condition?.text}
+                    width={'100px'}
                   />
-                  <div>
-                    Max Temp: {day.day.maxtemp_c} °C
-                    <br />
-                    Min Temp: {day.day.mintemp_c} °C
-                  </div>
+                </div>
+                <div className="card-footer text-muted d-flex justify-content-between">
+                  <span>
+                    Max {day.day.maxtemp_c} °C
+                  </span>
+                  <span>
+                    Min {day.day.mintemp_c} °C
+                  </span>
                 </div>
               </div>
             </div>
