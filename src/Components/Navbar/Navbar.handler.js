75cb5ad45d6b802071e498ea82440c0dec52c1a6
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2024-02-07 18:04:39 +0700

    change api link and style in mobile mode

--- src/Components/Navbar/Navbar.handler.js
+++ src/Components/Navbar/Navbar.handler.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const getSuggestLocation = async (searchKey) => {
   // API key default ini adalalh key personal yang hanya aktif sampai 12/Feb/2024, gunakan env variable untuk menggunakan key sendiri dengan menggunakan contoh .env.example
   const apiKey = process.env.API_KEY || "0cf0fea2677b42f899690517242901";
-  const suggestUrl = `/v1/search.json?q=${
+  const suggestUrl = `https://api.weatherapi.com/v1/search.json?q=${
     searchKey ? searchKey : "auto:ip"
   }&key=${apiKey}`;
 
